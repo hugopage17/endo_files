e285c92285fd78f2d02ee50c8a3784f799cd5137
hugopage17 <43106346+hugopage17@users.noreply.github.com>
2019-01-11 14:53:05 +1300

    Update server.py

--- server.py
+++ server.py
@@ -8,7 +8,7 @@ PORT = 8080
 Handler = http.server.SimpleHTTPRequestHandler
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print("serving at port", PORT)
+    print("Serving at port", PORT)
     sleep(1)
     print(Fore.BLUE + "Endo server is now running, type 'locahost:8080' in your browser to view your project")
     webbrowser.open('http://localhost:8080')
